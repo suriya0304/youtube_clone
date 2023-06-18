2be31719f06b58ebe1d37ebd77e0ea941c59e959
suriya0304 <ksuriya0304@gmail.com>
2023-06-18 14:43:01 +0530

    update docker compose with env vars

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,6 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: 'video-b627b.firebaseapp.com',
